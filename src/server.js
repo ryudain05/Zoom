5140ba8ac18ff084e0bfcf263f03458cceb40659
RDI <dkdlsekdls@naver.com>
2023-02-09 18:55:00 +0900

    feat/nicknames JSON->string/user

--- src/server.js
+++ src/server.js
@@ -27,11 +27,19 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "Anon";
   console.log("Connected to Browser");
   socket.on("close", onSocketClose);
-  socket.on("message", (message) => {
-    console.log(message.toString("utf-8"));
-    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
   });
 });
 
